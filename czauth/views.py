9f6b1c796f79903f21724b6409841c592e677dd0
Chen21yan <1170860535@qq.com>
2025-05-02 17:37:28 +0800

    数据库配置和验证码存储

--- czauth/views.py
+++ czauth/views.py
@@ -3,6 +3,7 @@ from django.http.response import JsonResponse
 import string
 import random
 from django.core.mail import send_mail
+from .models import CaptchaModel
 
 # Create your views here.
 
@@ -22,5 +23,7 @@ def send_email_captcha(request):
     # 生成验证码（取随机4位阿拉伯数字）
     # ['0', '2', '9', '4']
     captcha = "".join(random.sample(string.digits, 4))
+    # 存储到数据库中
+    CaptchaModel.objects.update_or_create(email=email, defaults={'captcha': captcha})
     send_mail("橙子博客注册验证码", message=f"您的注册验证码是:{captcha}", recipient_list=[email], from_email=None)
     return JsonResponse({'code': 200, 'message': '邮箱验证码发送成功！'})

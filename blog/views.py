78360de25f484ef6bbd85038d7ad600041617848
Chen21yan <1170860535@qq.com>
2025-05-03 15:04:25 +0800

    发布博客后端功能实现

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
 from django.urls.base import reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from .models import BlogCategory, Blog, BlogComment
+from .forms import PubBlogForm
 
 # Create your views here.
 def index(request):
@@ -14,6 +18,20 @@ def blog_detail(request, blog_id):
 # @login_required(login_url="/auth/login")
 
 # 需要在settings配置LOGIN_URL = '/auth/login'
+@require_http_methods(['GET', 'POST'])
 @login_required()
 def pub_blog(request):
-    return render(request, 'pub_blog.html')
+    if request.method == 'GET':
+        categories = BlogCategory.objects.all()
+        return render(request, 'pub_blog.html', context={'categories': categories})
+    else:
+        form = PubBlogForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            content = form.cleaned_data.get('content')
+            category_id = form.cleaned_data.get('category')
+            Blog.objects.create(title=title, content=content, category_id=category_id, author=request.user)
+            return JsonResponse({"code": 200, "message": "博客发布成功！"})
+        else:
+            print(form.errors)
+            return JsonResponse({"code": 400, "message": "参数错误！"})
